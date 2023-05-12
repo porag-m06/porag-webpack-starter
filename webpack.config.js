49edc6face20a5b776d3f93a97d10c619b740904
porag-m06 <porag.m06@gmail.com>
2023-05-12 14:49:51 +0600

    updated webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    //webpack entry and output config
     entry: {
         index: './src/index.js',
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-            template:'./src/index.html'
-        }),
-    ],
+
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+
+    //webpack plugins (e.g for HTML generation)
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./src/index.html'
+        }),
+    ],
+    
 };
